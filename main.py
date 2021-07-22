e03569ade6bb23d5fe3f058b8b01674cc45ddba7
Pavel Watson <watsonpavel@gmail.com>
2021-07-22 23:05:00 +0300

    Job posting website doesn't send a fully fledged ready HTML. It render the page dynamically using JavaScript and DOM API. So I can simply use a beautifulsoup for example to get the data I need. So I've made a function that extracts all the data I need(from a single page), I accomplished this using regular expressin and writing a good regex template, which seem to work well

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import ast
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def get_data():
-    only_with_salary_url = 'https://rabota.by/search/vacancy?area=1002&&specialization=1&text=Python&only_with_salary=true'
+    keyword = 'embedded'
+    only_with_salary_url = f'https://rabota.by/search/vacancy?area=1002&&specialization=1&text={keyword}&only_with_salary=true'
     page = requests.get(only_with_salary_url, headers=headers)
     return page
+
+
+def find_salaries(page):
+    regex = r'"compensation": {"from".+?"currencyCode": "..."|"compensation": {"to".+?"currencyCode": "..."'
+    pattern = re.compile(regex)
+    salaries = pattern.findall(page.text)
+    return salaries
+
+page = get_data()
+jobs = find_salaries(page)
+print(len(jobs))
