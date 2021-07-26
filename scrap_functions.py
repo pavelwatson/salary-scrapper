cb87584d3de65e4652b7de7c6a967e06cb023c60
Pavel Watson <watsonpavel@gmail.com>
2021-07-26 09:34:54 +0300

    removing all the code

--- scrap_functions.py
+++
@@ -1,6 +0,0 @@
-import re
-def scrap_data(html):
-    regex = r'"compensation": {"from".+?"currencyCode": "..."|"compensation": {"to".+?"currencyCode": "..."'
-    pattern = re.compile(regex)
-    data = pattern.findall(html)
-    return data
