c4ba8c04c01c34e04466ad53317e919b859e42d3
Pavel Watson <watsonpavel@gmail.com>
2021-07-27 13:35:47 +0300

    Added some cities to MANUALLY work with

--- request_functions.py
+++ request_functions.py
@@ -3,9 +3,12 @@ import requests
 
 def get_json(keyword, page_ind=0):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    id = 1
+    u_id = 1   # MOSCOW
+    # u_id = 113   # SANKT-PETERSBURG 
+    # u_id = 1002  # MINSK
+    # u_id = 115   # KIEV
     only_with_salary = 'true'
-    url = f'https://api.hh.ru/vacancies?&text={keyword}&area={id}&only_with_salary={only_with_salary}&page={page_ind}&specialization=1'
+    url = f'https://api.hh.ru/vacancies?&text={keyword}&area={u_id}&only_with_salary={only_with_salary}&page={page_ind}&specialization=1'
 
     response = requests.get(url, headers=headers)
     json_data = json.loads(response.text)
